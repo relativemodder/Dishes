a52e797fcba5c6d51c45c2730d273631491a0298
Student's PC <student@mrsu.ru>
2024-12-04 08:46:39 +0300

    readme updated

---
+++ DishesApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace DishesApp.Models
+{
+    public class User
+    {
+        public class Role
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public int Id { get; set; }
+        public required string Surname { get; set; }
+        public required string Name { get; set; }
+        public required string Patronymic { get; set; }
+        public required string Login { get; set; }
+        public required string Password { get; set; }
+        public required Role UserRole { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Dishes
+# DishesApp
+
+
+Переменные среды для подключения к БД:
+
+DISHES_DB_HOST = хост, по умолчанию localhost
+DISHES_DB_NAME = имя базы, по умолчанию dishes
+DISHES_DB_USER = имя пользователя для подключения, по умолчанию root
+DISHES_DB_PASSWORD = пароль пользователя, по умолчанию пустой
+
